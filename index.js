da0d60eeaa997071cbaaabca2f11be68aed13ecb
fayad  Mhamid <fayad6@walla.co.il>
2025-06-03 20:00:58 +0300

    updates

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ mongoose
 // Middlewares
 app.use(express.json());
 app.use(
-	cors({
-		origin: ["http://localhost:5173"],
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-	}),
+  cors({
+    origin: ["http://localhost:5173", "https://client-eta-liard.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 app.use(helmet());
 app.use(logger);
